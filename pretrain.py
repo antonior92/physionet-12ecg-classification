3b5d7a7b44a1ae32fdaf37ecb4e1959624104f1f
Antonio Horta Ribeiro <antonior92@gmail.com>
2020-04-22 18:36:32 -0300

    Pre-train script

---
+++ pretrain.py
@@ -0,0 +1,195 @@
+import os
+import json
+import torch
+import argparse
+import datetime
+import pandas as pd
+from warnings import warn
+from ecg_dataset import *
+from tqdm import tqdm
+import torch.nn as nn
+from torch.nn.utils import clip_grad_norm_
+from models.resnet import ResNet1d
+from metrics import get_metrics
+from output_layer import OutputLayer
+
+
+def get_input_and_targets(traces, args):
+    max_steps_ahead = max(args.k_steps_ahead)
+    inp = traces[:, :, :-max_steps_ahead]  # Try to predict k steps ahead
+    n = inp.size(2)
+    target = torch.cat([traces[:, :, k:k+n] for k in args.k_steps_ahead], dim=1)
+    return inp, target
+
+
+def selfsupervised(ep, model, optimizer, samples, loss, device, args, train):
+    if train:
+        model.train()
+    else:
+        model.eval()
+    total_loss = 0
+    n_entries = 0
+    n_total = len(samples)
+    n_batches = int(np.ceil(n_total/args.batch_size))
+    str_name = 'train' if train else 'val'
+    desc = "Epoch {:2d}: {} - Loss: {:.6f}"
+    bar = tqdm(initial=0, leave=True, total=n_total,
+               desc=desc.format(ep, str_name, 0), position=0)
+    for i in range(n_batches):
+        # Send to device
+        bs = min(args.batch_size, n_total - n_entries)
+        traces = torch.stack([torch.tensor(s['data'], dtype=torch.float32, device=device)
+                              for s in samples[n_entries:n_entries + bs]], dim=0)
+        inp, target = get_input_and_targets(traces, args)
+        if train:
+            # Reinitialize grad
+            model.zero_grad()
+            # Forward pass
+            output = model(inp)
+            ll = loss(output, target)
+            # Backward pass
+            ll.backward()
+            clip_grad_norm_(model.parameters(), args.clip_value)
+            # Optimize
+            optimizer.step()
+        else:
+            with torch.no_grad():
+                output = model(inp)
+                ll = loss(output, target)
+        # Update
+        total_loss += ll.detach().cpu().numpy()
+        n_entries += bs
+        # Update train bar
+        bar.desc = desc.format(ep, str_name, total_loss / n_entries)
+        bar.update(bs)
+    bar.close()
+    return total_loss / n_entries
+
+
+if __name__ == '__main__':
+    # Experiment parameters
+    config_parser = argparse.ArgumentParser(add_help=False)
+    config_parser.add_argument('--seed', type=int, default=2,
+                               help='random seed for number generator (default: 2)')
+    config_parser.add_argument('--epochs', type=int, default=200,
+                               help='maximum number of epochs (default: 70)')
+    config_parser.add_argument('--sample_freq', type=int, default=400,
+                               help='sample frequency (in Hz) in which all traces will be resampled at (default: 400)')
+    config_parser.add_argument('--seq_length', type=int, default=4096,
+                               help='size (in # of samples) for all traces. If needed traces will be zeropadded'
+                                    'to fit into the given size. (default: 4096)')
+    config_parser.add_argument('--batch_size', type=int, default=32,
+                               help='batch size (default: 32).')
+    config_parser.add_argument('--valid_split', type=float, default=0.30,
+                               help='fraction of the data used for validation (default: 0.1).')
+    config_parser.add_argument('--lr', type=float, default=0.001,
+                               help='learning rate (default: 0.001)')
+    config_parser.add_argument('--milestones', nargs='+', type=int,
+                               default=[75, 125, 175],
+                               help='milestones for lr scheduler (default: [100, 200])')
+    config_parser.add_argument("--lr_factor", type=float, default=0.1,
+                               help='reducing factor for the lr in a plateeu (default: 0.1)')
+    config_parser.add_argument('--dropout', type=float, default=0,
+                               help='dropout rate (default: 0).')
+    config_parser.add_argument('--kernel_size', type=int, default=17,
+                               help='kernel size in convolutional layers (default: 17).')
+    config_parser.add_argument('--rnn_type', type=str, default='LSTM',
+                               help="Which rnn to use. Options are {'LSTM', 'GRU', 'RNN'}")
+    config_parser.add_argument('--hidden_size', type=int, default=200,
+                               help="Hidden size rnn. Default is 200.")
+    config_parser.add_argument('--num_layers', type=int, default=1,
+                               help="Number of layers. Default is 1.")
+    config_parser.add_argument('--clip_value', type=float, default=0.25,
+                               help='maximum value for the gradient norm (default: 0.25)')
+    config_parser.add_argument('--k_steps_ahead', nargs='+', type=int, default=[10, 25, 75, 100],
+                               help='Try to predict k steps ahead')
+    config_parser.add_argument('--n_total', type=int, default=-1,
+                               help='number of samples to be used during training. By default use '
+                                    'all the samples available. Useful for quick tests.')
+
+    args, rem_args = config_parser.parse_known_args()
+    # System setting
+    sys_parser = argparse.ArgumentParser(add_help=False)
+    sys_parser.add_argument('--input_folder', type=str, default='./Training_WFDB',
+                            help='input folder.')
+    sys_parser.add_argument('--cuda', action='store_true',
+                            help='use cuda for computations. (default: False)')
+    sys_parser.add_argument('--folder', default=os.getcwd() + '/',
+                            help='output folder. If we pass /PATH/TO/FOLDER/ ending with `/`,'
+                                 'it creates a folder `output_YYYY-MM-DD_HH_MM_SS_MMMMMM` inside it'
+                                 'and save the content inside it. If it does not ends with `/`, the content is saved'
+                                 'in the folder provided.')
+
+    settings, unk = sys_parser.parse_known_args(rem_args)
+    #  Final parser is needed for generating help documentation
+    parser = argparse.ArgumentParser(parents=[sys_parser, config_parser])
+    _, unk = parser.parse_known_args(unk)
+    # Check for unknown options
+    if unk:
+        warn("Unknown arguments:" + str(unk) + ".")
+
+    # Set device
+    device = torch.device('cuda:0' if settings.cuda else 'cpu')
+    # Generate output folder if needed and save config file
+    if settings.folder[-1] == '/':
+        folder = os.path.join(settings.folder, 'output_' +
+                              str(datetime.datetime.now()).replace(":", "_").replace(" ", "_").replace(".", "_"))
+        try:
+            os.makedirs(folder)
+        except FileExistsError:
+            pass
+    else:
+        folder = settings.folder
+    with open(os.path.join(folder, 'pretrain_config.json'), 'w') as f:
+        json.dump(vars(args), f, indent='\t')
+    # Set seed
+    torch.manual_seed(args.seed)
+
+    tqdm.write("Define dataset...")
+    dset = ECGDataset(settings.input_folder, freq=args.sample_freq)
+    # Get length
+    n_total = len(dset) if args.n_total <= 0 else min(args.n_total, len(dset))
+    n_valid = int(n_total * args.valid_split)
+    n_train = n_total - n_valid
+    # Define dataset
+    train_dset = dset[:n_train]
+    train_samples = list(itertools.chain(*[split_long_signals(s) for s in train_dset]))
+    valid_dset = dset[n_train:n_total]
+    valid_samples = list(itertools.chain(*[split_long_signals(s) for s in valid_dset]))
+    # Get number of batches
+    tqdm.write("Done!")
+
+    tqdm.write("Define model...")
+    N_LEADS = 12
+
+    class MyRNN(nn.Module):
+        def __init__(self):
+            super(MyRNN, self).__init__()
+            self.rnn = getattr(nn, args.rnn_type)(N_LEADS, args.hidden_size, args.num_layers,
+                                                  dropout=args.dropout, batch_first=True)
+            self.linear = nn.Linear(args.hidden_size, N_LEADS * len(args.k_steps_ahead))
+
+        def forward(self, inp):
+            o1, _ = self.rnn(inp.transpose(1, 2))
+            o2 = self.linear(o1)
+            return o2.transpose(1, 2)
+
+    model = MyRNN()
+    model.to(device=device)
+    tqdm.write("Done!")
+
+    tqdm.write("Define optimizer...")
+    optimizer = torch.optim.Adam(model.parameters(), args.lr)
+    tqdm.write("Done!")
+
+    tqdm.write("Define scheduler...")
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.milestones, gamma=args.lr_factor)
+    tqdm.write("Done!")
+
+    tqdm.write("Define loss...")
+    loss = nn.MSELoss(reduction='sum')
+    tqdm.write("Done!")
+
+    for ep in range(args.epochs):
+        train_loss = selfsupervised(ep, model, optimizer, train_samples, loss, device, args, train=True)
+        valid_loss = selfsupervised(ep, model, optimizer, valid_samples, loss, device, args, train=False)
