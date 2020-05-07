2aff6438712671b113cb8c6bbd6fc47638118f6c
dgedon <daniel.gedon@gmx.de>
2020-05-07 18:26:59 +0200

    pretrain transformer added

--- pretrain.py
+++ pretrain.py
@@ -9,11 +9,15 @@ from warnings import warn
 from ecg_dataset import *
 from tqdm import tqdm
 import torch.nn as nn
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
 from torch.nn.utils import clip_grad_norm_
+import random
+import math
 
 
 class PretrainedRNNBlock(nn.Module):
     """Get reusable part from MyRNN and return new model. Include Linear block with the given output_size."""
+
     def __init__(self, pretrained, output_size, freeze=False):
         super(PretrainedRNNBlock, self).__init__()
         self.rnn = pretrained._modules['rnn']
@@ -30,11 +34,12 @@ class PretrainedRNNBlock(nn.Module):
 
 class MyRNN(nn.Module):
     """My RNN"""
+
     def __init__(self, args):
         super(MyRNN, self).__init__()
         N_LEADS = 12
         self.rnn = getattr(nn, args['rnn_type'])(N_LEADS, args['hidden_size'], args['num_layers'],
-                                              dropout=args['dropout'], batch_first=True)
+                                                 dropout=args['dropout'], batch_first=True)
         self.linear = nn.Linear(args['hidden_size'], N_LEADS * len(args['k_steps_ahead']))
 
     def forward(self, inp):
@@ -50,8 +55,131 @@ def get_input_and_targets(traces, args):
     max_steps_ahead = max(args.k_steps_ahead)
     inp = traces[:, :, :-max_steps_ahead]  # Try to predict k steps ahead
     n = inp.size(2)
-    target = torch.cat([traces[:, :, k:k+n] for k in args.k_steps_ahead], dim=1)
+    target = torch.cat([traces[:, :, k:k + n] for k in args.k_steps_ahead], dim=1)
     return inp, target
+
+
+class PositionalEncoding(nn.Module):
+    # This is the positional encoding according to paper "Attention is all you need".
+    # Could be changed to learnt encoding
+    r"""Inject some information about the relative or absolute position of the tokens
+        in the sequence. The positional encodings have the same dimension as
+        the embeddings, so that the two can be summed. Here, we use sine and cosine
+        functions of different frequencies.
+    .. math::
+        \text{PosEncoder}(pos, 2i) = sin(pos/10000^(2i/d_model))
+        \text{PosEncoder}(pos, 2i+1) = cos(pos/10000^(2i/d_model))
+        \text{where pos is the word position and i is the embed idx)
+    Args:
+        d_model: the embed dim (required).
+        dropout: the dropout value (default=0.1).
+        max_len: the max. length of the incoming sequence (default=10000).
+    """
+
+    def __init__(self, d_model, dropout=0.1, max_len=10000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        r"""Inputs of forward function
+        Args:
+            x: the sequence fed to the positional encoder model (required).
+        Shape:
+            x: [sequence length, batch size, embed dim]
+            output: [sequence length, batch size, embed dim]
+        Examples:
+            >>> output = pos_encoder(x)
+        """
+
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+
+# new pre-trained model
+class MyTransformer(nn.Module):
+    """My Transformer:
+    inspired by https://github.com/pytorch/examples/tree/master/word_language_model
+    """
+
+    def __init__(self, args):
+        super(MyTransformer, self).__init__()
+        self.N_LEADS = 12
+        self.mask_param = [args.num_masked_subseq, args.num_masked_samples]
+
+        self.pos_encoder = PositionalEncoding(args.emb_size, args.dropout)
+        self.encoder = nn.Linear(self.N_LEADS, args.emb_size)
+        encoder_layers = TransformerEncoderLayer(args.emb_size, args.num_heads, args.hidden_size, args.dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, args.num_trans_layers)
+        self.decoder = nn.Linear(args.emb_size, self.N_LEADS)
+
+        self.init_weights()
+
+    def _generate_random_sequence_mask(self, sz, param):
+        """
+        Implementation is quite inefficient so far. Should be improved!
+        Also the implementation does not care about overlapping intervals of masks.
+        This may yield that different number of samples are masked in different sequences.
+
+        According to attention definition the same mask is used for all sequences in the batch.
+        Mask is a [sz x sz] matrix. If the value [i,j] is masked by a value of -inf, then the for the
+        computation of output j the input i is masked, meaning that no attention is used for this input.
+
+        sz - sequence size
+        p - number of non-overlapping masked subsequences
+        m - number of consecutive samples for each p masked subsequences
+        """
+        p = param[0]
+        m = param[1]
+
+        # allocation
+        idx = torch.empty((sz, p * m), dtype=torch.int64)
+
+        # for all rows in the indexing
+        for i in range(sz):
+            # sample p values without replacement
+            a = random.sample(range(sz - m + 1), p)
+            a.sort()
+            idx_row = []
+            for k in range(p):
+                # generate indices for row i which should be masked
+                idx_row.extend(range(a[k], a[k] + m))
+            idx[i, :] = torch.tensor(idx_row)
+
+        # mask the indices with infinity
+        mask = torch.zeros(sz, sz)
+        mask.scatter_(1, idx, float('-inf'))
+        return mask
+
+    def init_weights(self):
+        # Comment: This is copied from the source. Not sure if necessary.
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src):
+        # src size is [batch size x N_LEADS x sequence length]
+        # transform src to be of size [sequence length x batch size x N_LEADS]
+        src = src.permute(2, 0, 1)
+        # generate random mask
+        mask = self._generate_random_sequence_mask(len(src), self.mask_param).to(device)
+        self.mask = mask
+
+        # process data
+        src = self.encoder(src) * math.sqrt(self.N_LEADS)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, self.mask)
+        output = self.decoder(output)
+        # permute to have the same dimensions as in the input
+        return output.permute(1, 2, 0)
 
 
 def selfsupervised(ep, model, optimizer, samples, loss, device, args, train):
@@ -62,23 +190,23 @@ def selfsupervised(ep, model, optimizer, samples, loss, device, args, train):
     total_loss = 0
     n_entries = 0
     n_total = len(samples)
-    n_batches = int(np.ceil(n_total/args.batch_size))
+    n_batches = int(np.ceil(n_total / args.batch_size))
     str_name = 'train' if train else 'val'
     desc = "Epoch {:2d}: {} - Loss: {:.6f}"
-    bar = tqdm(initial=0, leave=True, total=n_total,
-               desc=desc.format(ep, str_name, 0), position=0)
+    bar = tqdm(initial=0, leave=True, total=n_total, desc=desc.format(ep, str_name, 0), position=0)
     for i in range(n_batches):
         # Send to device
         bs = min(args.batch_size, n_total - n_entries)
         traces = torch.stack([torch.tensor(s['data'], dtype=torch.float32, device=device)
                               for s in samples[n_entries:n_entries + bs]], dim=0)
-        inp, target = get_input_and_targets(traces, args)
+        # reduces to make it run on my laptop for debugging
+        # traces = traces[:, :, :200]
         if train:
             # Reinitialize grad
             model.zero_grad()
             # Forward pass
-            output = model(inp)
-            ll = loss(output, target)
+            output = model(traces)
+            ll = loss(output, traces)
             # Backward pass
             ll.backward()
             clip_grad_norm_(model.parameters(), args.clip_value)
@@ -86,8 +214,8 @@ def selfsupervised(ep, model, optimizer, samples, loss, device, args, train):
             optimizer.step()
         else:
             with torch.no_grad():
-                output = model(inp)
-                ll = loss(output, target)
+                output = model(traces)
+                ll = loss(output, traces)
         # Update
         total_loss += ll.detach().cpu().numpy()
         n_entries += bs
@@ -121,14 +249,24 @@ if __name__ == '__main__':
                                help='milestones for lr scheduler (default: [100, 200])')
     config_parser.add_argument("--lr_factor", type=float, default=0.1,
                                help='reducing factor for the lr in a plateeu (default: 0.1)')
-    config_parser.add_argument('--dropout', type=float, default=0,
-                               help='dropout rate (default: 0).')
+    config_parser.add_argument('--dropout', type=float, default=0.2,
+                               help='dropout rate (default: 0.2).')
     config_parser.add_argument('--rnn_type', type=str, default='LSTM',
                                help="Which rnn to use. Options are {'LSTM', 'GRU', 'RNN'}")
-    config_parser.add_argument('--hidden_size', type=int, default=800,
-                               help="Hidden size rnn. Default is 800.")
+    config_parser.add_argument('--hidden_size', type=int, default=50,
+                               help="Hidden size rnn. Default is 50.")
     config_parser.add_argument('--num_layers', type=int, default=1,
                                help="Number of layers. Default is 1.")
+    config_parser.add_argument('--num_heads', type=int, default=2,
+                               help="Number of attention heads. Default is 2.")
+    config_parser.add_argument('--num_trans_layers', type=int, default=2,
+                               help="Number of transformer blocks. Default is 2.")
+    config_parser.add_argument('--emb_size', type=int, default=20,
+                               help="Embedding size for transformer. Default is 50.")
+    config_parser.add_argument('--num_masked_subseq', type=int, default=6,
+                               help="Number of attention masked subsequences. Default is 75.")
+    config_parser.add_argument('--num_masked_samples', type=int, default=8,
+                               help="Number of attention masked consecutive samples. Default is 8.")
     config_parser.add_argument('--clip_value', type=float, default=1.0,
                                help='maximum value for the gradient norm (default: 1.0)')
     config_parser.add_argument('--k_steps_ahead', nargs='+', type=int, default=[10, 20, 25, 50, 75, 90, 100],
@@ -204,7 +342,7 @@ if __name__ == '__main__':
     tqdm.write("Done!")
 
     tqdm.write("Define model...")
-    model = MyRNN(vars(args))
+    model = MyTransformer(args)  # MyRNN(vars(args))
     model.to(device=device)
     tqdm.write("Done!")
 
@@ -220,7 +358,7 @@ if __name__ == '__main__':
     loss = nn.MSELoss(reduction='sum')
     tqdm.write("Done!")
 
-    history = pd.DataFrame(columns=["epoch", "train_loss", "valid_loss", "lr",])
+    history = pd.DataFrame(columns=["epoch", "train_loss", "valid_loss", "lr", ])
     best_loss = np.Inf
     for ep in range(args.epochs):
         train_loss = selfsupervised(ep, model, optimizer, train_samples, loss, device, args, train=True)
