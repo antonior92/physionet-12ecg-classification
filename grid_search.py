63765af7a4557a4086cd62268edec5c5aa2a85d2
Danielmtex <danielmartins.2008@gmail.com>
2020-06-04 17:11:22 -0300

    Update grid_search.py

--- grid_search.py
+++ grid_search.py
@@ -26,16 +26,16 @@ def execute(trial):
 
 
     os.chdir(main_path)
-    setup =('python train.py --epochs 10 --folder "{}\\outputs gridsearch\\iteration{}"'.format(main_path,i),
-        '--kernel_size {}'.format(trial.suggest_int('kernel_size', 3, 35)), 
+    setup =('python train.py --n_total 12 --epochs 10 --folder "{}\\outputs gridsearch\\iteration{}"'.format(main_path,i),
+        '--kernel_size {}'.format(trial.suggest_int('kernel_size', 3, 36)), 
         '--dropout_rate {}'.format(trial.suggest_float('dropout_rate', 0.001, 1.0)),
         '--lr_factor {}'.format(trial.suggest_loguniform('lr_factor',0.0001,1)),
         '--lr {}'.format(trial.suggest_loguniform('lr', 0.0001, 1)) ,
         '--batch_size {}'.format(trial.suggest_int('batch_size', 15, 300)))
 
     cmd = " ".join(setup)
-    os.system(cmd)
-    #subp.check_call(cmd, shell=True)
+    #os.system(cmd)
+    subp.check_call(cmd, shell=True)
     #reads csv file
     geom_mean = geom_mean_searcher(i)
 
@@ -56,7 +56,7 @@ def geom_mean_searcher(i):
 
 #defining search space
 search_space = {
-    'kernel_size': [5, 10, 17, 34],
+    'kernel_size': [5, 11, 17, 35],
     'dropout_rate': [0.3, 0.5, 0.7 ],
     'lr_factor': [0.1],
     'lr': [0.001, 0.01],
@@ -76,9 +76,11 @@ print('The best value was:',study.best_value)
 
 
 #todo: 
-#fix .join's
+#testar hiperparametros
+#adicionar pretreino
+#deletar modelos para não estourar memória
 #argparse para simplificar codigo
-#testar codigo
+
 
 """
 Done:
@@ -87,4 +89,6 @@ figure how to wait until the csv file is complete to run the file (line 35) -> a
 define the parameters range for the optimization
 figure out how to pass i as a working argument (used trial._trial_id to solve problem)
 finish writing the code to show the results for each parameter and the best result found
+#fix .join's
+#testar codigo
 """
