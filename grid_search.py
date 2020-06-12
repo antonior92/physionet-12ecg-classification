4943b0400ecd81774429f6d0facaf2b1fea25911
Danielmtex <danielmartins.2008@gmail.com>
2020-06-12 13:13:20 -0300

    Update grid_search.py

--- grid_search.py
+++ grid_search.py
@@ -18,24 +18,34 @@ def execute(trial):
     if os.path.isdir("".join((main_path,'\\outputs gridsearch'))) == False:
         os.mkdir('outputs gridsearch')
     os.chdir("".join((main_path,'\\outputs gridsearch')))
-    #create directory for each iteration with a specific name that can later be accessed, already created
-    #on train.py
+    #create directory for each iteration with a specific name that can later be accessed( removed because train.py already creates it)
     #if os.path.exists('iteretion{}'.format(i)):
     #    shutil.rmtree('iteretion{}'.format(i))
     #os.makedirs('iteretion{}'.format(i))
 
 
     os.chdir(main_path)
-    setup =('python train.py --n_total 12 --epochs 10 --folder "{}\\outputs gridsearch\\iteration{}"'.format(main_path,i),
+    #pretrain.py setup
+    pre_set_up = ('python pretrain.py --folder "{}\\outputs gridsearch\\iteration{}"'.format(main_path,i),
+        '--lr {}'.format(trial.suggest_loguniform('pre_lr', 0.0001, 1)), 
+        '--lr_factor {}'.format(trial.suggest_loguniform('pre_lr_factor',0.0001,1)), 
+        '--dropout {}'.format(trial.suggest_float('pre_dropout_rate', 0.001, 1.0)),
+        '--num_heads {}'.format(trial.suggest_int('pre_num_heads', 5, 11)))
+    #train.py setup
+    train_set_up =('python train.py --n_total 12 --epochs 10 --folder "{}\\outputs gridsearch\\iteration{}"'.format(main_path,i),
         '--kernel_size {}'.format(trial.suggest_int('kernel_size', 3, 36)), 
         '--dropout_rate {}'.format(trial.suggest_float('dropout_rate', 0.001, 1.0)),
         '--lr_factor {}'.format(trial.suggest_loguniform('lr_factor',0.0001,1)),
         '--lr {}'.format(trial.suggest_loguniform('lr', 0.0001, 1)) ,
         '--batch_size {}'.format(trial.suggest_int('batch_size', 15, 300)))
-
-    cmd = " ".join(setup)
-    #os.system(cmd)
-    subp.check_call(cmd, shell=True)
+    
+    #pretrain comand
+    pre_cmd = " ".join(pre_set_up)
+    subp.check_call(pre_cmd, shell=True)
+    #train command
+    train_cmd = " ".join(train_set_up)
+    subp.check_call(train_cmd, shell=True)
+    #os.system(cmd) caso falhe
     #reads csv file
     geom_mean = geom_mean_searcher(i)
 
@@ -56,17 +66,24 @@ def geom_mean_searcher(i):
 
 #defining search space
 search_space = {
-    'kernel_size': [5, 11, 17, 35],
+    'pre_lr':[0.01,0.001],
+    'pre_lr_factor':[0.1],
+    'pre_dropout_rate':[0.1, 0.2 , 0.3],
+    'pre_num_heads':[6,8,10],
+    'kernel_size': [11, 17, 35],
     'dropout_rate': [0.3, 0.5, 0.7 ],
     'lr_factor': [0.1],
     'lr': [0.001, 0.01],
     'batch_size': [32 ,64]
 }
-
+#calculates number of trials
+number_trials=1
+for key, value in search_space.items() :
+    number_trials *= len(value)
 
 #sets up the study and calls the function
 study= optuna.create_study(sampler=optuna.samplers.GridSampler(search_space), direction='maximize')
-study.optimize(execute, n_trials=4*3*1*2*2)
+study.optimize(execute, n_trials = number_trials)
 joblib.dump(study, dump_file_path)
 
 #afterexecuting shows best parameters
