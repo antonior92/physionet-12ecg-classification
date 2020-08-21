2878a721cd7101988f771ca08c806f49fc81093f
Antonio Horta Ribeiro <antonior92@gmail.com>
2020-08-21 19:56:24 -0300

    Update train_12ECG_classifier to move downloaded folders

--- train_12ECG_classifier.py
+++ train_12ECG_classifier.py
@@ -6,8 +6,14 @@ import os
 
 def train_12ECG_classifier(input_directory, output_directory):
     sys.path.extend(['./'])
-    cmd = 'python train.py --train_classes scored --valid_classes scored --seed 3 --out_layer challenge_sigmoid ' \
-          '--valid_split 0 --save_last --input_folder {:} --folder {:}'.format(input_directory,
-                                                                               output_directory.strip('/'))
+    folder = 'multiple/'
+    for subfolder in [os.path.join(folder, f) for f in os.listdir(folder)]:
+        cmd = 'cp -r {} {}'.format(subfolder, output_directory.strip('/'))
+        print(cmd)
+        os.system(cmd)
+    cmd = 'python train.py --train_classes scored --valid_classes scored --seed 12 --out_layer challenge_sigmoid ' \
+          '--valid_split 0 --save_last --input_folder {:} --folder {:}'.format(
+        input_directory, os.path.join(output_directory.strip('/'), 'latest_model'))
+
     print(cmd)
     os.system(cmd)
